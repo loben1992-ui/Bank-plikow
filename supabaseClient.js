11c9b14aa92b178ecbaaf6a5d80a2e98520f60ae
Lukasz <loben1992@gmail.com>
2025-09-04 10:20:13 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 // Ten plik musi być w tym samym folderze
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://cvghbxnxnhjjuzkebpey.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z2hieG54bmhqanV6a2VicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5NjM1MDksImV4cCI6MjA3MjUzOTUwOX0.8h1qWukprv_ozQiZRZO_3PyzGrVkZft6j47MIpbIJ0o';
+//const supabaseUrl = 'https://cvghbxnxnhjjuzkebpey.supabase.co';
+//const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z2hieG54bmhqanV6a2VicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5NjM1MDksImV4cCI6MjA3MjUzOTUwOX0.8h1qWukprv_ozQiZRZO_3PyzGrVkZft6j47MIpbIJ0o';
 
  Wersja dla Vercel/Netlify (odkomentuj i użyj zamiast tej powyżej):
  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
@@ -10,4 +10,5 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
 
